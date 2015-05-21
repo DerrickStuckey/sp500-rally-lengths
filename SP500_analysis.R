28ea9320a90073cb500de3d03638fcc2c2ef05ff
DerrickStuckey <derrickstuckey@gmail.com>
2015-05-21 13:59:45 -0400

    updated file paths and comments

--- SP500_analysis.R
+++ SP500_analysis.R
@@ -2,13 +2,15 @@
 #derrickstuckey@gmail.com
 #S&P 500 rally/correction analysis
 
+library(MASS)
+
 #Constants
 correctionFactor = 0.9
 rallyFactor = (1.0/0.9)
 rallyType = "rally"
 correctionType = "correction"
 
-sp500_history <- read.csv("~/Desktop/Kaggle/Stock Market/Rproj/sp500_history.csv")
+sp500_history <- read.csv("./sp500_history.csv")
 #sort the data chronologically
 sp500_history <- sp500_history[rev(rownames(sp500_history)),]
 
@@ -20,6 +22,7 @@ runs <- data.frame("StartDate"=sp500_history$Date[1], "EndDate"=sp500_history$Da
 
 inCorrection <- function(runs,j) {runs$Type[j] == correctionType}
 
+#partition time period into a set of rallies and a set of corrections
 i <- 1
 j <- 1
 for (i in 1:dim(sp500_history)[1]) {
@@ -69,10 +72,21 @@ rallyLengths <- as.numeric(rallies$EndDate) - as.numeric(rallies$StartDate)
 
 #fit and plot exponential distribution
 rallyLengthsExpEst <- fitdistr(rallyLengths, "exponential")$estimate
-hist(rallyLengths, breaks=30, prob=TRUE, main="Probability Density of Rally Lengths", xlab="Days", ylab="Probability Density")
+hist(rallyLengths, breaks=30, prob=TRUE, main="Probability Density of Rally Lengths", xlab="Rally Length (Days)", ylab="Probability")
 curve(dexp(x, rate = rallyLengthsExpEst), add = TRUE, col = "blue", lwd = 2)
 
 #fit and plot weibull distribution
-library(MASS)
 weibullFit <- fitdistr(rallyLengths, "weibull")
 curve(dweibull(x, shape=weibullFit$estimate[1], scale=weibullFit$estimate[2]), add = TRUE, col = "red", lwd = 2)
+
+#fit log normal dist and plot
+#lognormFit <- fitdistr(rallyLengths, "lognormal")
+#curve(dlnorm(x, meanlog=lognormFit$estimate[1], sdlog=lognormFit$estimate[2]), add = TRUE, col = "green", lwd = 2)
+
+#draw example normal curve
+curve(dnorm(x,mean=3,sd=1),from=0,to=6,col="blue",xlab="Length of Rally",ylab="Probability",frame.plot=TRUE,axes=FALSE)
+#draw example log-normal curve
+curve(dlnorm(x,meanlog=2,sdlog=0.5),from=0,to=25,col="blue",xlab="Length of Rally",ylab="Probability",frame.plot=TRUE,axes=FALSE)
+#draw example log-normal curve
+curve(dexp(x,rate=1),from=0,to=6,col="blue",xlab="Length of Rally",ylab="Probability",frame.plot=TRUE,axes=FALSE)
+
